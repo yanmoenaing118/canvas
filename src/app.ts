4193e79c95caa153189a3b49bb2eeee22fa7c7db
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-03-13 19:47:09 +0630

    push changes

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ const { width: w, height: h } = canvas;
 let delta: number = 0,
   totalTime: number = 0;
 
-
 function draw(ellapsedTime: number) {
   delta = (ellapsedTime - totalTime) * 0.001;
   totalTime = ellapsedTime;
@@ -12,33 +11,26 @@ function draw(ellapsedTime: number) {
   ctx.clearRect(0, 0, w, h);
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 
-  ctx.save();
-  ctx.fillStyle = "darkred";
-  ctx.textAlign = "center";
+
   animateText(ctx, delta);
-  ctx.restore();
-  
-  ctx.save();
-  ctx.fillStyle = 'red';
-  ctx.translate(w/2,h/2)
-  ctx.fillRect(0,0,100,100);
-  ctx.restore();
 
+//   ctx.save();
+//   ctx.fillStyle = "red";
+//   ctx.translate(w / 2, h / 2);
+//   ctx.fillRect(0, 0, 100, 100);
+//   ctx.restore();
 
+//   ctx.save();
+//   ctx.fillStyle = "green";
+//   ctx.translate(100, 100);
+//   ctx.fillRect(0, 0, 100, 100);
+//   ctx.restore();
 
-  ctx.save();
-  ctx.fillStyle = 'green';
-  ctx.translate(100,100);
-  ctx.fillRect(0,0,100,100);
-  ctx.restore();
-
-
-  ctx.save();
-  ctx.fillStyle = 'pink';
-  ctx.translate(100,0);
-  ctx.fillRect(0,0,100,100);
-  ctx.restore();
-
+//   ctx.save();
+//   ctx.fillStyle = "pink";
+//   ctx.translate(100, 0);
+//   ctx.fillRect(0, 0, 100, 100);
+//   ctx.restore();
 
   requestAnimationFrame(draw);
 }
@@ -51,16 +43,24 @@ let textOpacity = 0;
 function animateText(ctx: CanvasRenderingContext2D, delta: number) {
   textX = w / 2;
   textY = h / 2;
-  if(textSize > 20) {
-    textSize -= 1;
+  if (textSize > 20) {
+    textSize -= 0.5;
   }
-  if(textOpacity < 1) {
-    textOpacity += 0.05;
+  if (textOpacity < 1) {
+    textOpacity += 0.01;
   }
+
+
+  ctx.save();
+  ctx.fillStyle = "darkred";
+  ctx.textAlign = "center";
+
   ctx.globalAlpha = textOpacity;
   ctx.font = `${textSize}px Arial`;
-  ctx.fillText("Hello Canvas", textX, textY);
+  ctx.translate(textX, textY);
+  ctx.fillText("Hello Canvas", 0, 0);
 
+  ctx.restore();
 }
 
 requestAnimationFrame(draw);
