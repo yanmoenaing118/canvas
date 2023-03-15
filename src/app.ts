5afe9b27c5152995408ed393883a417c01849ab5
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-03-15 21:03:49 +0630

    detect selected objects

--- src/app.ts
+++ src/app.ts
@@ -8,46 +8,43 @@ class Rect {
   pos: Position;
   width: number;
   height: number;
-
+  selected: boolean = false;
   constructor(width: number, height: number, fill: string) {
     this.width = width;
     this.height = height;
     this.fill = fill;
     this.pos = { x: 0, y: 0 };
   }
+
+  setSelected(selected: boolean) {
+    this.selected = selected;
+  }
 }
 
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-let selectedObject: any;
+let selectedObject: Rect | null;
 let threshold: number = 10;
 
-// canvas.addEventListener("mousemove", function (e: MouseEvent) {
-//   if (
-//     checkInsideRect(
-//       {
-//         x: e.clientX,
-//         y: e.clientY,
-//       },
-//       rect
-//     )
-//   ) {
-//     console.log(rect);
-//   }
-// });
 
 canvas.addEventListener("click", function (e: MouseEvent) {
-  if (
-    checkInsideRect(
-      {
-        x: e.clientX,
-        y: e.clientY,
-      },
-      rect
-    )
-  ) {
-    console.log(rect);
-  }
+  rects.forEach((rect) => {
+    if (
+      checkInsideRect(
+        {
+          x: e.clientX,
+          y: e.clientY,
+        },
+        rect
+      )
+    ) {
+      rect.setSelected(true);
+      selectedObject = rect;
+    } else {
+      rect.setSelected(false);
+      selectedObject = null;
+    }
+  });
 });
 
 function checkInsideRect(clickedPos: Position, rect: Rect) {
@@ -71,18 +68,28 @@ const rect = new Rect(100, 100, "pink");
 rect.pos.x = canvas.width / 2 - 50;
 rect.pos.y = canvas.height / 2 - 50;
 
+const rectTwo = new Rect(50, 50, "red");
+
+const rects: Rect[] = [];
+
+rects.push(rect);
+rects.push(rectTwo);
 function draw(currentTimestamp: number) {
   requestAnimationFrame(draw);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  /** update */
-
-  /** render */
-  ctx.save();
-  ctx.translate(rect.pos.x, rect.pos.y);
-  ctx.fillStyle = rect.fill;
-  ctx.fillRect(0, 0, rect.width, rect.height);
-  ctx.restore();
+  rects.forEach((rect) => {
+    ctx.save();
+    ctx.translate(rect.pos.x, rect.pos.y);
+    ctx.fillStyle = rect.fill;
+    if (rect.selected) {
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+      ctx.strokeRect(0, 0, rect.width, rect.height);
+    }
+    ctx.fillRect(0, 0, rect.width, rect.height);
+    ctx.restore();
+  });
 
   delta = (currentTimestamp - lastTimeStamp) * 0.001;
   lastTimeStamp = currentTimestamp;
