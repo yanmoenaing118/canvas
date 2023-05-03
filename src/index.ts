e74797ac7d8bc5419e3615dbf4807e6977296f2d
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-05-03 11:42:52 +0630

    new game

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
+import Game from "./pop/Game";
 import pop from "./pop/index";
 const { CanvasRenderer, Container, KeyControls, Text, Texture, Sprite } = pop;
 
 // Game setup code
 const w = 640;
 const h = 300;
-const renderer = new CanvasRenderer(w, h);
-document.querySelector("#board")?.appendChild(renderer.view);
+const game = new Game(w, h);
 
 // Load game textures
 const textures = {
@@ -18,7 +18,7 @@ const textures = {
 console.log(textures)
 
 // Game objects
-const scene = new Container();
+const scene = game.scene;
 const controls = new KeyControls();
 
 // Make a spaceship
@@ -95,68 +95,55 @@ let spawnSpeed = 1.0;
 let scoreAmount = 0;
 let gameOver = false;
 
-// Main loop
-let dt = 0;
-let last = 0;
+game.run((dt: number, t: number) => {
 
-function loopy(ms: number) {
-  requestAnimationFrame(loopy);
-
-  const t = ms / 1000;
-  dt = t - last;
-  last = t;
-
-  // Game logic code
-  ship.pos.x += Math.sin(t * 10); // "bob" the player
-  score.text = "score: " + scoreAmount; // update score
-
-  // Check if player can fire yet
-  if (!gameOver && controls.action && t - lastShot > 0.15) {
-    lastShot = t;
-    fireBullet(ship.pos.x + 24, ship.pos.y + 10);
-  }
-
-  // Check for collisions, or out of screen
-  baddies.children.forEach(baddie => {
-    bullets.children.forEach(bullet => {
-
-      // Check distance between baddie and bullet
-      const dx = baddie.pos.x + 16 - (bullet.pos.x + 8);
-      const dy = baddie.pos.y + 16 - (bullet.pos.y + 8);
-      if (Math.sqrt(dx * dx + dy * dy) < 24) {
-        // A hit!
-        bullet.dead = true;
-        baddie.dead = true;
-        scoreAmount += Math.floor(t);
-      }
-      // Bullet out of the screen?
-      if (bullet.pos.x >= w + 20) {
-        bullet.dead = true;
-      }
-    });
-
-    // Check if baddie reached the city
-    if (baddie.pos.x < -32) {
-      if (!gameOver) {
-        doGameOver();
-      }
-      baddie.dead = true;
-    }
-  });
-
-  // Spawn a baddie
-  if (t - lastSpawn > spawnSpeed) {
-    lastSpawn = t;
-    const speed = -50 - Math.random() * Math.random() * 100;
-    const position = Math.random() * (h - 24);
-    spawnBaddie(w, position, speed);
-
-    // Accelerating for the next spawn
-    spawnSpeed = spawnSpeed < 0.05 ? 0.6 : spawnSpeed * 0.97 + 0.001;
-  }
-
-  // Update & render everything
-  scene.update(dt, t);
-  renderer.render(scene);
-}
-requestAnimationFrame(loopy);
+  console.log(dt);
+   // Game logic code
+   ship.pos.x += Math.sin(t * 10); // "bob" the player
+   score.text = "score: " + scoreAmount; // update score
+ 
+   // Check if player can fire yet
+   if (!gameOver && controls.action && t - lastShot > 0.15) {
+     lastShot = t;
+     fireBullet(ship.pos.x + 24, ship.pos.y + 10);
+   }
+ 
+   // Check for collisions, or out of screen
+   baddies.children.forEach(baddie => {
+     bullets.children.forEach(bullet => {
+ 
+       // Check distance between baddie and bullet
+       const dx = baddie.pos.x + 16 - (bullet.pos.x + 8);
+       const dy = baddie.pos.y + 16 - (bullet.pos.y + 8);
+       if (Math.sqrt(dx * dx + dy * dy) < 24) {
+         // A hit!
+         bullet.dead = true;
+         baddie.dead = true;
+         scoreAmount += Math.floor(t);
+       }
+       // Bullet out of the screen?
+       if (bullet.pos.x >= w + 20) {
+         bullet.dead = true;
+       }
+     });
+ 
+     // Check if baddie reached the city
+     if (baddie.pos.x < -32) {
+       if (!gameOver) {
+         doGameOver();
+       }
+       baddie.dead = true;
+     }
+   });
+ 
+   // Spawn a baddie
+   if (t - lastSpawn > spawnSpeed) {
+     lastSpawn = t;
+     const speed = -50 - Math.random() * Math.random() * 100;
+     const position = Math.random() * (h - 24);
+     spawnBaddie(w, position, speed);
+ 
+     // Accelerating for the next spawn
+     spawnSpeed = spawnSpeed < 0.05 ? 0.6 : spawnSpeed * 0.97 + 0.001;
+   }
+})
--- src/pop/renderer/CanvasRenderer.ts
+++ src/pop/renderer/CanvasRenderer.ts
@@ -1,64 +1,63 @@
 import Container from "../Container";
 
 class CanvasRenderer {
-    w: number;
-    h: number;
-    view: HTMLCanvasElement;
-    ctx: CanvasRenderingContext2D;
-    constructor(w: number, h: number) {
-      const canvas = document.createElement("canvas");
-      this.w = canvas.width = w;
-      this.h = canvas.height = h;
-      this.view = canvas;
-      this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-      this.ctx.imageSmoothingEnabled = false;
-      this.ctx.textBaseline = "top";
-    }
-  
-    render(container: Container, clear = true) {
-      if (container.visible == false) {
-        return;
-      }
-      const { ctx } = this;
-  
-      function renderRec(container: Container) {
-        // Render the container children
-        container.children.forEach(child => {
-          if (child.visible == false) {
-            return;
-          }
-          ctx.save();
-  
-          // Handle transforms
-          if (child.pos) {
-            ctx.translate(Math.round(child.pos.x), Math.round(child.pos.y));
-          }
-  
-          // Draw the leaf nodes
-          if (child.text) {
-            const { font, fill, align } = child.style;
-            if (font) ctx.font = font;
-            if (fill) ctx.fillStyle = fill;
-            if (align) ctx.textAlign = align;
-            ctx.fillText(child.text, 0, 0);
-          } else if (child.texture) {
-            ctx.drawImage(child.texture.img, 0, 0);
-          }
-  
-          // Render any child sub-nodes
-          if (child.children) {
-            renderRec(child);
-          }
-          ctx.restore();
-        });
-      }
-  
-      if (clear) {
-        ctx.clearRect(0, 0, this.w, this.h);
-      }
-      renderRec(container);
-    }
+  w: number;
+  h: number;
+  view: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  constructor(w: number, h: number) {
+    const canvas = document.createElement("canvas");
+    this.w = canvas.width = w;
+    this.h = canvas.height = h;
+    this.view = canvas;
+    this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.ctx.imageSmoothingEnabled = false;
+    this.ctx.textBaseline = "top";
   }
-  
-  export default CanvasRenderer;
-  
+
+  render(container: Container, clear = true) {
+    if (container.visible == false) {
+      return;
+    }
+    const { ctx } = this;
+
+    function renderRec(container: Container) {
+      // Render the container children
+      container.children.forEach((child) => {
+        if (child.visible == false) {
+          return;
+        }
+        ctx.save();
+
+        // Handle transforms
+        if (child.pos) {
+          ctx.translate(Math.round(child.pos.x), Math.round(child.pos.y));
+        }
+
+        // Draw the leaf nodes
+        if (child.text) {
+          const { font, fill, align } = child.style;
+          if (font) ctx.font = font;
+          if (fill) ctx.fillStyle = fill;
+          if (align) ctx.textAlign = align;
+          ctx.fillText(child.text, 0, 0);
+        } else if (child.texture) {
+          ctx.drawImage(child.texture.img, 0, 0);
+        }
+
+        // Render any child sub-nodes
+        if (child.children) {
+          renderRec(child);
+        }
+        ctx.restore();
+      });
+    }
+
+    if (clear) {
+      ctx.clearRect(0, 0, this.w, this.h);
+    }
+    renderRec(container);
+  }
+}
+
+export default CanvasRenderer;
