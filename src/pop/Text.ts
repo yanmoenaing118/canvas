dcf2c846503897d9b96d770cb60b0f0d3d009ee5
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-05-10 15:55:26 +0630

    save

--- src/pop/Text.ts
+++ src/pop/Text.ts
@@ -1,15 +1,14 @@
 import { Position } from "./models";
 
 class Text {
-    pos: Position;
-    text: string;
-    style: any;
-    constructor(text: string = "", style = {}) {
-      this.pos = { x: 0, y: 0 };
-      this.text = text;
-      this.style = style;
-    }
+  pos: Position;
+  text: string;
+  style: any;
+  constructor(text: string = "", style = {}) {
+    this.pos = { x: 0, y: 0 };
+    this.text = text;
+    this.style = style;
   }
-  
-  export default Text;
-  
+}
+
+export default Text;
