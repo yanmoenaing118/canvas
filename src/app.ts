3a3b4599ca1d36831393c392ed79cc88cd30cb82
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-05-21 10:37:12 +0630

    tst

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,8 @@ import { Position } from "./types";
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 const { width, height } = canvas;
-const rangeEl = document.querySelector('input') as HTMLInputElement;
-rangeEl.value = '0';
+const rangeEl = document.querySelector("input") as HTMLInputElement;
+rangeEl.value = "0";
 
 const img = new Image();
 img.src = "./spaceship.png";
@@ -13,49 +13,44 @@ let w = 32;
 let h = 32;
 
 let t = 0;
-let dt = 1/60;
+let dt = 1 / 60;
 
 let rotation = 0;
 
 let anchor: Position = {
-    x: -16,
-    y: -16
-}
+  x: -16,
+  y: -16,
+};
 
 function loop(lastEllapsedTime: number) {
-    context.clearRect(0,0,width,height);
-    context.save();
-    context.fillStyle = "red";
-    context.clearRect(0, 0, width, height);
-  
-    context.fillStyle = "green";
-    //   context.rotate(Math.PI / 4);
-  
-    context.translate(width / 2, height / 2);
-    rotation += 0.05;
-    context.rotate(rotation);
-    context.scale(-1.5,1.5);
-    // context.fillRect((w / 2) * -1, (h / 2) * -1, w, h);
-    context.translate(anchor.x, anchor.y);
-    context.drawImage(img, 0, 0);
-    context.restore();
-    requestAnimationFrame(loop);
+  context.clearRect(0, 0, width, height);
+  context.save();
+  context.fillStyle = "red";
+  context.clearRect(0, 0, width, height);
+
+  context.fillStyle = "green";
+  //   context.rotate(Math.PI / 4);
+
+  context.translate(width / 2, height / 2);
+  rotation += 0.05;
+  context.rotate(rotation);
+  context.scale(-1.5, 1.5);
+  // context.fillRect((w / 2) * -1, (h / 2) * -1, w, h);
+  context.translate(anchor.x, anchor.y);
+  context.drawImage(img, 0, 0);
+  context.restore();
+  requestAnimationFrame(loop);
 }
 
 function updateRotation() {
-    // rotation += ;
+  // rotation += ;
 }
 
-rangeEl.addEventListener('input', function() {
-    rotation =  Number(this.value);
-    console.log(rotation);
-})
-
+rangeEl.addEventListener("input", function () {
+  rotation = Number(this.value);
+  console.log(rotation);
+});
 
 window.onload = function () {
-    requestAnimationFrame(loop);
-  };
-  
-  
-  
-  
+  requestAnimationFrame(loop);
+};
