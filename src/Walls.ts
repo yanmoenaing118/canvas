3345ce17a8db03b66290806adc2061caac68eb97
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-06-15 22:37:21 +0630

    camer :(

--- src/Walls.ts
+++ src/Walls.ts
@@ -1,15 +1,26 @@
+import Camera from "./Camera";
+
 export default class Wall {
   walls: { x: number; y: number; w: number; h: number; color: string }[] = [];
-  constructor(w: number, h: number, wallSize: number) {
+  camera: Camera;
+  constructor(w: number, h: number, wallSize: number, camera: Camera) {
     this.walls = [
       { x: 0, y: 0, w: w, h: wallSize, color: "red" },
       { x: w - wallSize, y: 0, w: wallSize, h: h, color: "green" },
       { x: 0, y: h - wallSize, w: wallSize, h: wallSize, color: "blue" },
       { x: 0, y: 0, w: wallSize, h: h - wallSize, color: "yellow" },
     ];
+    this.camera = camera;
   }
 
-  update(dt: number) {}
+  update(dt: number) {
+    const [top, right, bottom, left] = this.walls
+    left.x = dt * 200 * this.camera.pos.x;
+    right.x =   ( dt * 200 * this.camera.pos.x );
+    
+    top.y = dt * 200 * this.camera.pos.y;
+    bottom.y =  (dt * 200 * this.camera.pos.y);
+  }
 
   render(ctx: CanvasRenderingContext2D) {
     this.walls.forEach((wall) => {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import Camera from "./Camera";
 import KeyControls from "./KeyControls";
+import Player from "./Player";
 import Wall from "./Walls";
 import { clamp } from "./helpers";
 
@@ -27,29 +29,20 @@ let world = {
   h: h * 2,
 };
 
-let camera = {
-  pos: { x: 0, y: 0 },
-  w: w,
-  h: h,
-  offset: { x: 0, y: 0 },
-};
-
-let player = {
-  pos: { x: 0, y: 0 },
-  w: 32,
-  h: 32,
-};
-
-let playerBounds = {
-  left: wallSize,
-  right: camera.w - wallSize,
-  top: wallSize,
-  bottom: camera.h - wallSize,
-};
+// let playerBounds = {
+//   left: wallSize,
+//   right: camera.w - wallSize,
+//   top: wallSize,
+//   bottom: camera.h - wallSize,
+// };
 
 // top / right / bottom / left
+const camera = new Camera(w, h, { w: world.w, h: world.h });
+const walls = new Wall(world.w, world.h, wallSize, camera);
+const player = new Player(wallSize, wallSize, controls);
 
-const walls = new Wall(world.w, world.h, wallSize);
+// focus player
+camera.setSubject(player);
 
 const loop = (ellapsedTime: number) => {
   requestAnimationFrame(loop);
@@ -60,7 +53,13 @@ const loop = (ellapsedTime: number) => {
   ctx.strokeStyle = "black";
   ctx.strokeRect(0, 0, w, h);
 
+  camera.update(dt);
+  walls.update(dt);
+  player.update(dt);
+
+  camera.render(ctx);
   walls.render(ctx);
+  player.render(ctx);
 };
 
 requestAnimationFrame(loop);
