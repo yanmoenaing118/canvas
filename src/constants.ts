f6144f0da61af97dfdcea2a2af07ba448967f7b3
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-06-17 22:38:11 +0630

    ch width

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const MAX_FRAME = 1 / 60;
 export const SPEED = 1640;
 export const CELLSIZE = 64;
 export let WIDTH = Math.round(window.innerWidth / CELLSIZE) * CELLSIZE;
-export let HEIGHT = Math.round(window.innerHeight / CELLSIZE ) * CELLSIZE;
+export let HEIGHT = Math.round(window.innerHeight / CELLSIZE ) *  CELLSIZE;
 
 console.log(WIDTH,HEIGHT)
 export const controls = new KeyControls();
