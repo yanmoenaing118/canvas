8a42b3516c44b1e0cdae8f49da4633575a45407a
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-06-18 16:31:15 +0630

    game screen

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,15 @@
-import Squizz from "./pop/entities/Squizz";
 import Game from "./pop/Game";
 import pop from "./pop/index";
-import Level from "./Level";
-import Camera from "./pop/Camera";
-import Container from "./pop/Container";
-import Baddie from "./pop/entities/Baddie";
-import entity from "./pop/utils/entities";
-import TileSprite from "./pop/TileSprite";
+import GameScreen from "./screens/GameScree";
 const { KeyControls, math } = pop;
 
 const w = 640;
 const h = 480;
 const game = new Game(w, h);
-
 const controls = new KeyControls();
 
-const scene = game.scene;
+const gameScreen = new GameScreen(game,controls);
 
-const wrold = {
-  w: w + w * 0.5,
-  h: w + h * 0.4,
-};
+game.scene = gameScreen;
+game.run();
 
-const level = new Level(wrold.w, wrold.h);
-const squizz = new Squizz(controls);
-squizz.pos.x = w / 2 - squizz.w * 0.5;
-squizz.pos.y = h / 2 - squizz.h * 0.5;
-const camera = new Camera(squizz, { w, h }, wrold);
-const baddies = addBaddies(level);
-
-camera.add(level);
-camera.add(squizz);
-camera.add(baddies);
-scene.add(camera);
-
-function addBaddies(level: Level): Container {
-  const baddies = new Container();
-  for (let i = 0; i < 5; i++) {
-    const b = baddies.add(new Baddie(32 * 5, 0));
-    b.pos.y = Math.floor(level.h / 5) * i + level.tileH * 2;
-  }
-  for (let i = 0; i < 10; i++) {
-    const b = baddies.add(new Baddie(0, 32 * 5));
-    b.frame = { x: 1, y: 0 };
-    b.pos.x = Math.floor(level.w / 10) * i + level.tileW;
-  }
-  return baddies;
-}
-
-function updateBaddies() {
-  baddies.map((b: Baddie) => {
-    const { pos } = b;
-    if (entity.distance(squizz as TileSprite, b as TileSprite) < 32) {
-      squizz.dead = true;
-      if (b.xSpeed) pos.x = -level.w;
-      else pos.y = -level.h;
-    }
-
-    if (pos.x > level.w) pos.x = -32;
-    if (pos.y > level.h) pos.y = -32;
-  });
-}
-
-game.run((dt: number, t: number) => {
-  squizz.pos.x = math.clamp(
-    squizz.pos.x,
-    level.bounds.left,
-    level.bounds.right
-  );
-  squizz.pos.y = math.clamp(
-    squizz.pos.y,
-    level.bounds.top,
-    level.bounds.bottom
-  );
-
-  updateBaddies();
-
-  const ground = level.checkGround(squizz.pos);
-  if(ground == 'cleared') {
-    squizz.dead = true;
-  }
-});
---
+++ src/screens/GameScree.ts
@@ -0,0 +1,97 @@
+import Level from "../Level";
+import Camera from "../pop/Camera";
+import Container from "../pop/Container";
+import Game from "../pop/Game";
+import TileSprite from "../pop/TileSprite";
+import KeyControls from "../pop/controls/KeyControls";
+import Baddie from "../pop/entities/Baddie";
+import Squizz from "../pop/entities/Squizz";
+import entity from "../pop/utils/entities";
+import math from "../pop/utils/math";
+
+export default class GameScreen extends Container {
+  private world = {
+    w: 0,
+    h: 0,
+  };
+
+  game: Game;
+  controls: KeyControls;
+  level: Level;
+  squizz: Squizz;
+  baddies: Container;
+
+  constructor(game: Game, controls: KeyControls) {
+    super();
+    this.game = game;
+    this.controls = controls;
+    this.world = {
+      w: game.w + game.w * 0.5,
+      h: game.h + game.h * 0.4,
+    };
+    this.level = new Level(this.world.w, this.world.h);
+    this.squizz = new Squizz(controls);
+    this.squizz.pos.x = game.w / 2 - this.squizz.w * 0.5;
+    this.squizz.pos.y = game.h / 2 - this.squizz.h * 0.5;
+    this.baddies = this.addBaddies(this.level);
+    
+    const camera = this.add(new Camera(this.squizz,{
+        w: this.game.w,
+        h: this.game.h,
+    }, this.world));
+
+    camera.add(this.level);
+    camera.add(this.squizz);
+    camera.add(this.baddies)
+
+}
+
+  addBaddies(level: Level): Container {
+    const baddies = new Container();
+    for (let i = 0; i < 5; i++) {
+      const b = baddies.add(new Baddie(32 * 5, 0));
+      b.pos.y = Math.floor(level.h / 5) * i + level.tileH * 2;
+    }
+    for (let i = 0; i < 10; i++) {
+      const b = baddies.add(new Baddie(0, 32 * 5));
+      b.frame = { x: 1, y: 0 };
+      b.pos.x = Math.floor(level.w / 10) * i + level.tileW;
+    }
+    return baddies;
+  }
+
+  updateBaddies() {
+    this.baddies.map((b: Baddie) => {
+      const { pos } = b;
+      if (entity.distance(this.squizz as TileSprite, b as TileSprite) < 32) {
+        this.squizz.dead = true;
+        if (b.xSpeed) pos.x = -this.level.w;
+        else pos.y = -this.level.h;
+      }
+
+      if (pos.x > this.level.w) pos.x = -32;
+      if (pos.y > this.level.h) pos.y = -32;
+    });
+  }
+
+  update(dt: number, t: number): void {
+    super.update(dt,t); // Don't forget to call super otherwise children won't run thier update method
+    this.squizz.pos.x = math.clamp(
+        this.squizz.pos.x,
+        this.level.bounds.left,
+        this.level.bounds.right
+      );
+      this.squizz.pos.y = math.clamp(
+        this.squizz.pos.y,
+        this.level.bounds.top,
+        this.level.bounds.bottom
+      );
+    
+      this.updateBaddies();
+    
+      const ground = this.level.checkGround(this.squizz.pos);
+      if(ground == 'cleared') {
+        this.squizz.dead = true;
+      }
+  }
+}
