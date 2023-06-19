9b420626b87ecc11e9e1e2327f45b32207c6b592
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-06-19 20:48:14 +0630

    save

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import Entity from "./Entity";
+
 /**
  *
  * @param milli
@@ -7,7 +9,13 @@ export function sec(milli: number) {
   return milli * 0.001;
 }
 
+export function clamp(x: number, min: number, max: number) {
+  return Math.max(min, Math.min(x, max));
+}
 
-export function clamp(x: number,min: number, max: number) {
-  return Math.max(min, Math.min(x,max));
+export function distance(e1: Entity, e2: Entity) {
+  return Math.sqrt(
+    (e1.pos.x - e2.pos.x) * (e1.pos.x - e2.pos.x) +
+      (e1.pos.y - e2.pos.y) * (e1.pos.y - e2.pos.y)
+  );
 }
