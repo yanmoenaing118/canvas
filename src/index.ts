7e3566b4dac51fe6e6ae0ba3b6b8d50a13b82a5f
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-06-20 20:54:40 +0630

    tile sprite init

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import KeyControls from "./KeyControls";
 import { renderGrid } from "./DebugGrid";
 import { CELLSIZE, HEIGHT, MAX_FRAME, SPEED, WIDTH } from "./constants";
 import { clamp, distance } from "./utils";
-import Rect from "./Rect";
-import { renderImg, renderRect } from "./renderers";
-import Sprite from "./Sprite";
+
 
 const canvas = document.createElement("canvas") as HTMLCanvasElement;
 document.body.appendChild(canvas);
@@ -21,37 +19,7 @@ let dt = 1 / 60;
 let time = 0;
 
 
-const rect2 = new Rect();
-rect2.style.fill = "red";
-rect2.w = CELLSIZE * 2;
-rect2.h = CELLSIZE * 2;
-rect2.center(w, h);
 
-function updateRect(dt: number) {
-
-}
-
-const img = new Image();
-img.src = "logo.ico";
-const love = new Sprite(img, CELLSIZE * 2, CELLSIZE * 2);
-love.center(w,h);
-
-function updateSprite(dt: number) {
-    const isFlip = controls.x == -1;
-    if(isFlip) {
-      console.log(love.pos);
-      love.anchor = { x: love.w, y: 0 }
-      love.scale.x = controls.x;
-    } else if(love.scale.x == -1 && controls.x == 1) {
-      love.anchor = { x: 0, y: 0};
-      love.scale.x = 1;
-      console.log('hi')
-    }
-
-    console.log(love.anchor, love.scale);
-
-   love.pos.x += dt * 100 * controls.x;
-}
 
 function loop(ellapsedTime: number) {
   requestAnimationFrame(loop);
@@ -60,12 +28,6 @@ function loop(ellapsedTime: number) {
   time = ellapsedTime;
   ctx.clearRect(0, 0, w, h);
   ctx.globalAlpha = 0.6;
-
-  updateRect(dt);
-  updateSprite(dt);
-
-  renderRect(rect2, ctx);
-  renderImg(love, ctx);
 
 
   renderGrid(h / cellSize, w / cellSize, cellSize, cellSize);
