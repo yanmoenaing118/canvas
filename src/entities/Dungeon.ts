14123eb7d8d1409c802670e06efffa0da6b32f3d
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-07-03 21:52:50 +0630

    random map: TODO -> to fix

--- src/entities/Dungeon.ts
+++ src/entities/Dungeon.ts
@@ -5,7 +5,7 @@ import { Frame } from "../pop/models";
 import math from "../pop/utils/math";
 
 const mapW = 20;
-const mapH = 12;
+const mapH = 8;
 const tileSize = TILE_SIZE;
 const texture = new Texture("images/bravedigger-tiles.png");
 
@@ -14,22 +14,45 @@ function getById(id: string | number): any {
 }
 
 const tileIndexes = [
-  { id: "empty", x: 0, y: 2 },
-  { id: "wall", x: 2, y: 2 },
-  { id: "wall_end", x: 3, y: 2 },
-];
-
+    { id: "empty", x: 0, y: 2 },
+    { id: "wall", x: 2, y: 2 },
+    { id: "wall_end", x: 3, y: 2 }
+  ];
 class Dungeon extends TileMap {
   constructor() {
-    const level = new Array(mapW * mapH).fill(2);
+    const level = new Array(mapW * mapH).fill("empty");
 
+    console.log('x,y,index')
     for (let y = 0; y < mapH; y++) {
       for (let x = 0; x < mapW; x++) {
-        level[y * mapW + x] = math.randOneFrom(["empty", "empty", "wall"]);
+        if (y === 0 || x === 0 || y === mapH - 1 || x === mapW - 1) {
+          level[y * mapW + x] = "wall";
+          continue;
+        }
+
+        console.log(x,y, y * mapW + x)
+
+        if(y % 2 || x % 2 || math.rand(2)) {
+            continue;
+        } 
+ 
+        level[y * mapW + x] = "wall"
+   
+        const [xo, yo] = math.randOneFrom([[0, -1], [0, 1], [1, 0], [-1,
+            0]]);
+        level[(y + yo) * mapW + (x + xo)] = "wall_end";
+        
       }
     }
 
-    super(level.map(i => getById(i)), mapW, mapH, tileSize, tileSize, texture);
+    super(
+      level.map((i) => getById(i)),
+      mapW,
+      mapH,
+      tileSize,
+      tileSize,
+      texture
+    );
   }
 }
 
