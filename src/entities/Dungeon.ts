5805b715ad582221a8e2bd060f3b3ccdcf5d5d56
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-07-04 21:10:25 +0630

    dungeon done

--- src/entities/Dungeon.ts
+++ src/entities/Dungeon.ts
@@ -14,22 +14,23 @@ function getById(id: string | number): any {
 }
 
 const tileIndexes = [
-    { id: "empty", x: 0, y: 2 },
-    { id: "wall", x: 2, y: 2 },
-    { id: "wall_end", x: 3, y: 2 }
-  ];
+  { id: "empty", x: 0, y: 2 },
+  { id: "wall", x: 2, y: 2 },
+  { id: "wall_end", x: 3, y: 2 },
+];
 class Dungeon extends TileMap {
   constructor() {
     const level = new Array(mapW * mapH).fill("empty");
 
-    console.log('x,y,index')
+    console.log("x,y,index");
     for (let y = 0; y < mapH; y++) {
       for (let x = 0; x < mapW; x++) {
         const index = y * mapW + x;
-        console.log(x,y,index)
+        console.log(x, y, index);
 
-        if(y == 0 || x == 0 || y == mapH - 1 || x == mapW - 1) {
-          level[index] = 'wall'
+        if (y == 0 || x == 0 || y == mapH - 1 || x == mapW - 1) {
+          level[index] = "wall";
+          continue;
         }
 
         /**
@@ -37,8 +38,29 @@ class Dungeon extends TileMap {
          * if 1 skip the rest and continue the next loop
          * if 0 continue the rest
          */
-        if(math.randOneIn(3)) { 
+        if (y % 2 || x % 2 || math.rand(4)) {
           continue;
+        }
+
+        level[index] = "wall";
+
+        const [xo, yo] = math.randOneFrom([
+          [0, -1],
+          [0, 1],
+          [1, 0],
+          [-1, 0],
+        ]);
+        level[(y + yo) * mapW + (x + xo)] = "wall";
+      }
+    }
+
+    // "3d-ify" if no wall below a tile
+    for (let y = 0; y < mapH - 1; y++) {
+      for (let x = 0; x < mapW; x++) {
+        const below = level[(y + 1) * mapW + x];
+        const me = level[y * mapW + x];
+        if (me === "wall" && below !== "wall") {
+          level[y * mapW + x] = "wall_end";
         }
       }
     }
