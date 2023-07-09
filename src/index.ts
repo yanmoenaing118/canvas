05cab601842c676f57537da33e46f205bf15c7a0
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-07-09 09:42:40 +0630

    palyer pos

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import Player from "./entities/Player";
 import Container from "./pop/Container";
 import Game from "./pop/Game";
 import Rect from "./pop/Rect";
+import Text from "./pop/Text";
 import pop from "./pop/index";
 import entities from "./pop/utils/entities";
 import math from "./pop/utils/math";
@@ -17,40 +18,51 @@ const h = CANVAS_HEIGHT;
 const game = new Game(w, h);
 const { scene } = game;
 
-
-
 const dungeon = scene.add(new Dungeon());
 const player = new Player(controls);
+const playerStatus = new Text(player.pos.x + " " + player.pos.y, {
+  font: "24px Arial",
+  fill: "white",
+});
 const bounds = entities.bounds(player);
+scene.add(playerStatus);
 
 const boundRects = dungeon.tilesAtCorners(bounds, 0, 0).map((tile) => {
-  const r = scene.add(new Rect(tile.w, tile.h, {
-    fill: 'rgba(3,55,5,0.3)'
-  }));
+  const r = scene.add(
+    new Rect(tile.w, tile.h, {
+      fill: "rgba(3,222,5,0.3)",
+    })
+  );
   r.pos.x = tile.pos.x;
   r.pos.y = tile.pos.y;
   return r;
 });
 
-scene.add(player);
 
 player.pos.x = player.w;
 player.pos.y = player.h;
 
+const playerRect = scene.add(new Rect(player.w,player.h, {
+  fill: 'white'
+}))
+playerRect.pos.x = player.pos.x;
+playerRect.pos.y = player.pos.y; 
 
-
-
-
-// console.log(player.pos);
-// console.log(bounds);
-// console.log(collidingTiles.map((t) => t.frame));
-
+scene.add(player);
 game.run(() => {
+  const playerMapPos = dungeon.pixelToMapPosition(player.pos);
+  playerStatus.text = `
+    pixel: (${player.pos.x.toFixed(2)} ${player.pos.y.toFixed(2)})
+    map: (${playerMapPos.x}, ${playerMapPos.y})
+    `;
+  
+    playerRect.pos = player.pos;
+
   const bounds = entities.bounds(player);
-  const collidingTiles = dungeon.tilesAtCorners(bounds, -player.w * 0.4, -player.h * 0.4);
+  const collidingTiles = dungeon.tilesAtCorners(bounds, 0, 0);
   collidingTiles.forEach((tile, index) => {
-    if(!tile) return;
+    if (!tile) return;
     boundRects[index].pos.x = tile.pos.x;
     boundRects[index].pos.y = tile.pos.y;
-  })
+  });
 });
