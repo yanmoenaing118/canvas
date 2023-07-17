cfff9763c91ed760cc54b13729d3bffe21eadcff
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-07-17 20:06:50 +0630

    inbox collision

--- src/Entity.ts
+++ src/Entity.ts
@@ -1,34 +1,26 @@
-import Vec2 from "./Vec2";
-import { CELLSIZE } from "./constants";
-import { Styles } from "./types";
+import { Vec2 } from "./interfaces";
 
-export default class Entity {
+class Entity {
   pos: Vec2;
-  anchor: Vec2;
-  scale: Vec2;
-  rotation: number;
-  style: Styles;
   w: number;
   h: number;
-  constructor() {
+  fill: string;
+
+  constructor(w: number, h: number, fill: string) {
     this.pos = { x: 0, y: 0 };
-    this.anchor = { x: 0, y: 0 };
-    this.scale = { x: 1, y: 1 };
-    this.rotation = 0;
-    this.style = {};
-    this.w = CELLSIZE;
-    this.h = CELLSIZE;
+    this.w = w;
+    this.h = h;
+    this.fill = fill;
   }
 
-  update(dt: number, t: number): void {}
-
-  /**
-   * 
-   * @param w container width
-   * @param h container heigh
-   */
-  center(w: number, h: number) {
-    this.pos.x = w / 2 - this.w * 0.5;
-    this.pos.y = h / 2 - this.h * 0.5;
+  render(ctx: CanvasRenderingContext2D) {
+    ctx.save();
+    ctx.translate(this.pos.x, this.pos.y);
+    ctx.fillRect(0, 0, this.w, this.h);
+    ctx.restore();
   }
+
+  
 }
+
+export default Entity;
