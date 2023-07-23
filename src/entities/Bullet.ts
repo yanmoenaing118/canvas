8011c6a5a657e253307bb88fae164864c6630091
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-07-23 09:39:55 +0630

    shoot bullet

---
+++ src/entities/Bullet.ts
@@ -0,0 +1,19 @@
+import { TILE_SIZE } from "../constants";
+import Texture from "../pop/Texture";
+import TileSprite from "../pop/TileSprite";
+import { Position } from "../pop/models";
+const texture = new Texture("images/bravedigger-tiles.png");
+export default class Bullet extends TileSprite {
+  speed: number;
+  dir: Position;
+  constructor(dir: Position, speed = 100) {
+    super(texture, TILE_SIZE, TILE_SIZE);
+    this.speed = speed;
+    this.dir = dir;
+  }
+
+  update(dt: number): void {
+    this.pos.x += Math.cos(this.dir.x) * dt * this.speed;
+    this.pos.y += Math.sin(this.dir.y) * dt * this.speed;
+  }
+}
