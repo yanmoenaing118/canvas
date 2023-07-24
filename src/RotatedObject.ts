bd51754eb9fee968d732c282cdd360d742c9fe14
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-07-24 21:05:15 +0630

    practice

--- src/RotatedObject.ts
+++ src/RotatedObject.ts
@@ -4,15 +4,15 @@ export default class RotatedObject {
   pos = { x: 0, y: 0 };
   rotation = 0;
   scale = { x: 1, y: 1 };
-  w = CELLSIZE;
-  h = CELLSIZE;
+  w = CELLSIZE * 0.5;
+  h = CELLSIZE * 0.5;
 
   anchor = { x: 0, y: 0 };
   pivot = { x: 0, y: 0 };
 
   constructor() {
-    this.pos.x = WIDTH / 2;
-    this.pos.y = HEIGHT / 2;
+    // this.pos.x = WIDTH / 2;
+    // this.pos.y = HEIGHT / 2;
 
     this.pivot.x = this.w / 2;
     this.pivot.y = this.h / 2;
