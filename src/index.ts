21e7bba471e9f04d0d2cc8e404eeb52da517aa46
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-16 12:32:41 +0630

    save constants

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import KeyControls from "./KeyControls";
 import RotatedObject from "./RotatedObject";
 
 import { CELLSIZE, HEIGHT, MAX_DELTA, WIDTH } from "./constants";
+import { clamp } from "./utils";
 
 const canvas = document.createElement("canvas") as HTMLCanvasElement;
 export const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 document.body.appendChild(canvas);
-canvas.style.margin = "30px";
+// canvas.style.margin = "30px";
 canvas.style.boxShadow = "0 0 3px rgba(0,0,0,0.3)";
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
@@ -23,6 +24,10 @@ let dt = 0;
 let t = 0;
 
 const rotated = new RotatedObject();
+const r = new RotatedObject();
+let scale = 1;
+
+r.pos.x = CELLSIZE * 2;
 
 function loop(ellapsedTime: number) {
   dt = Math.min(MAX_DELTA, (ellapsedTime - t) * 0.001);
@@ -31,10 +36,23 @@ function loop(ellapsedTime: number) {
   ctx.clearRect(0, 0, WIDTH + CELLSIZE, HEIGHT + CELLSIZE);
 
   rotated.update(dt, t);
+  r.update(dt,t);
+
+  ctx.save();
+
+  ctx.scale(scale, scale);
 
   rotated.render(ctx);
+  r.render(ctx);
   renderGrid(ctx, HEIGHT / CELLSIZE, WIDTH / CELLSIZE, CELLSIZE, CELLSIZE);
+  
+  ctx.restore();
   requestAnimationFrame(loop);
 }
+
+canvas.addEventListener('wheel', e => {
+  scale += e.deltaY * -0.01;
+  scale = clamp(scale, 1, 3);
+})
 
 requestAnimationFrame(loop);
