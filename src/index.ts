7b9641263db277bf1fff3ead2812d779fa9a207a
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-17 19:41:36 +0630

    GameScreen

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import { CANVAS_HEIGHT, CANVAS_WIDTH, TILE_SIZE } from "./constants";
-import Bat from "./entities/Bat";
-import Bullet from "./entities/Bullet";
-import Dungeon from "./entities/Dungeon";
-import Player from "./entities/Player";
-import Totem from "./entities/Totem";
-import Container from "./pop/Container";
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./constants";
 import Game from "./pop/Game";
-import Text from "./pop/Text";
 import pop from "./pop/index";
-import math from "./pop/utils/math";
 import GameScreen from "./screens/GameScreen";
 const { KeyControls } = pop;
 
@@ -16,7 +8,6 @@ const controls = new KeyControls();
 const w = CANVAS_WIDTH;
 const h = CANVAS_HEIGHT;
 const game = new Game(w, h);
-const { scene } = game;
 
 game.scene = new GameScreen(game, controls);
 game.run();
