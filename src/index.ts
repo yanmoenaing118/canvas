e888287b98fb4ebe0e9bf9f4e5877e94328b69bd
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-22 19:42:41 +0630

    timeline second block

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,86 @@
-import { renderGrid } from "./DebugGrid";
-import KeyControls from "./KeyControls";
-import RotatedObject from "./RotatedObject";
-
-import { CELLSIZE, HEIGHT, MAX_DELTA, WIDTH } from "./constants";
-import { clamp } from "./utils";
-const container = document.createElement("div");
-const canvasWrapper = document.createElement("div");
-
-container.classList.add("container");
-document.body.appendChild(container);
-container.appendChild(canvasWrapper);
-
-const canvas = document.createElement("canvas") as HTMLCanvasElement;
+const canvas  = document.createElement("canvas");
+document.body.appendChild(canvas);
 export const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-canvasWrapper.appendChild(canvas);
-// canvas.style.margin = "30px";
-canvas.style.boxShadow = "0 0 3px rgba(0,0,0,0.3)";
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
-const dpr = window.devicePixelRatio;
-const rect = canvas.getBoundingClientRect();
-canvas.width = rect.width * dpr;
-canvas.height = rect.height * dpr;
-ctx.scale(dpr, dpr);
-canvas.style.width = `${rect.width}px`;
-canvas.style.height = `${rect.height}px`;
 
-let dt = 0;
-let t = 0;
-
-const rotated = new RotatedObject();
-const r = new RotatedObject();
-let scale = 1;
-
-r.pos.x = CELLSIZE * 4;
-r.pos.y = CELLSIZE * 3;
-let x = 0;
-let y = 0;
-
-canvas.addEventListener("mousemove", (e) => {
-  x = e.pageX;
-  y = e.pageY;
-  console.log(x, y);
-});
+const w = window.innerWidth;
+const h = 100;
 
 
-let wrapperWidth = WIDTH;
-canvas.addEventListener("wheel", (e) => {
-  e.preventDefault();
-  // if (e.deltaX > 0) return;
-  scale += e.deltaY * -0.005;
-  scale = clamp(scale, 1, 3);
+canvas.width = w;
+canvas.height = h;
 
 
-  wrapperWidth += scale;
+setResolution(canvas, ctx);
 
-  canvas.style.width = `${wrapperWidth}px`;
+let unit = "s";
+let seconds = 60;
+let secondGap = 10; // 10s
+let tenSecondGap = 100; // 100px
+let timeLineLength = (seconds / secondGap) * tenSecondGap;
+let totalBlock = Math.floor(timeLineLength / tenSecondGap);
+
+let unitLgLineLength = 15;
+let unitLgLineWidth = 1;
 
 
-  // canvasWrapper.style.width = `${wrapperWidth}px`;
-});
+let timeFormat = "00:00"; // mm:ss
 
-requestAnimationFrame(loop);
+console.log(totalBlock);
 
-function loop(ellapsedTime: number) {
-  dt = Math.min(MAX_DELTA, (ellapsedTime - t) * 0.001);
-  t = ellapsedTime;
-
-  ctx.clearRect(0, 0, WIDTH + CELLSIZE, HEIGHT + CELLSIZE);
-
-  rotated.update(dt, t);
-  r.update(dt, t);
-
-  ctx.save();
-  ctx.translate(x, y);
-  ctx.scale(scale, 1);
-
-  ctx.save();
-  ctx.translate(-x, -y);
+function getTimeFormat(timeGap: number, index: number, unit: string) {
+  let divider = 1;
+  let min = 0;
+  let sec = 0;
+  if(unit == "s") {
+    divider = 60;
+    min = Math.floor((index * timeGap) / divider);
+    sec = Math.floor((index * timeGap) % divider);
+  }
   
-  console.log(Math.floor(parseInt(canvas.style.width) / CELLSIZE))
+
+  return `${min}:${sec}`;
+}
+
+let i = 0;
+function loop(){ 
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+
+
+  for(i = 0; i < totalBlock + 1; i++){ 
+    const x = i * tenSecondGap;
+    const y = 0;
+    const time = getTimeFormat(secondGap, i, "s");
+    ctx.save();
+    ctx.lineWidth = unitLgLineWidth;
+    ctx.translate(x, y);
+    ctx.fillText(time, 4, 12);
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(0, unitLgLineLength);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.restore();
+  }
   
-  rotated.render(ctx);
-  r.render(ctx);
-  renderGrid(ctx, Math.floor(parseInt(canvas.style.width) / CELLSIZE), 3, CELLSIZE, CELLSIZE);
-
-  ctx.restore();
-  ctx.restore();
-
-
 
 
   requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+
+
+function setResolution(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+  const dpr = window.devicePixelRatio;
+  const rect = canvas.getBoundingClientRect();
+
+  canvas.width = dpr * rect.width;
+  canvas.height  = dpr * rect.height;
+
+  ctx.scale(dpr, dpr);
+  canvas.style.width = `${rect.width}px`;
+  canvas.style.height = `${rect.height}px`;
+
+  canvas.style.borderBottom = '1px solid black';
 }
