c7e7abf6200ebac1aa4d5edb00a365e6cb60ef15
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-09-03 21:15:30 +0630

    practice

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import { renderGrid } from "./DebugGrid";
 import Render from "./Render";
-import { CELLSIZE, HEIGHT, MAX_DELTA, WIDTH } from "./constants";
+import { CELLSIZE, HEIGHT, WIDTH } from "./constants";
 
 const { ctx } = new Render();
 
 let dt = 0;
 let t = 0;
-
-let sum = []
-
-function cpuIntensive() {
-
-}
 
 function loop(ellapsedTime: number) {
   dt = (ellapsedTime - t) * 0.001;
@@ -24,8 +18,6 @@ function loop(ellapsedTime: number) {
   renderGrid(ctx, HEIGHT / CELLSIZE, WIDTH / CELLSIZE, CELLSIZE, CELLSIZE);
 
 
-  cpuIntensive();
-  console.log(`${dt}`);
   requestAnimationFrame(loop);
 }
 
