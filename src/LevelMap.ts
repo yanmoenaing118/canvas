7dfee9a7f46c19c3a20b3bce36036e56e06f27c5
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-09-30 14:44:00 +0630

    ascii codes

--- src/LevelMap.ts
+++ src/LevelMap.ts
@@ -3,28 +3,54 @@ import TileMap from "./TileMap";
 import { h, w } from "./canvas";
 import { TileSpriteFrame } from "./classes";
 
+function getTile(char: string) {
+  switch (char) {
+    case "#":
+      return { x: 8, y: 0 };
+    case "B":
+      return { x: 9, y: 2 };
+    case "C":
+      return { x: 1, y: 6 };
+    case "M":
+      return { x: 4, y: 2 };
+    case "D":
+      return { x: 5, y: 1 };
+    case "E":
+      return { x: 4, y: 0 };
+    default:
+      return { x: 1, y: 1 };
+  }
+}
 export default class LevelMap extends TileMap {
   constructor() {
     const tileW = 64;
     const tileH = 64;
 
+    const ascii = `
+##########
+#        #
+#        #
+#        #
+#        #
+#        #
+#        #
+BBBBBBBBBB`;
+
     const mapW = w / tileW;
     const mapH = h / tileH;
-    const level: TileSpriteFrame[] = [];
+    let level: TileSpriteFrame[] = [];
 
-    for(let i = 0; i < mapH; i++) {
-      for(let j = 0 ; j < mapW; j++) {
-        const index = (i * mapW) + j;
-        level[index] = {
-          x: Math.round(Math.random() * mapW),
-          y: Math.round(Math.random() * mapH)
-        }
+    const cells = ascii
+      .split("\n")
+      .slice(1)
+      .map((row) => row.split(""));
+
+    for (let i = 0; i < cells.length; i++) {
+      for (let j = 0; j < cells[i].length; j++) {
+        level[i * cells[i].length + j] = getTile(cells[i][j]);
       }
     }
 
-    console.log(level);
     super(level, new Texture("dungeon.png"), mapW, mapH, tileW, tileH);
   }
-
-
 }
