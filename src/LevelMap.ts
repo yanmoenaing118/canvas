e946b76d5722940df1fa79230b1cf4a87737a8a9
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-09-30 14:55:01 +0630

    player pos

--- src/LevelMap.ts
+++ src/LevelMap.ts
@@ -1,7 +1,7 @@
 import Texture from "./Texture";
 import TileMap from "./TileMap";
 import { h, w } from "./canvas";
-import { TileSpriteFrame } from "./classes";
+import { TileSpriteFrame, Vec2 } from "./classes";
 
 function getTile(char: string) {
   switch (char) {
@@ -16,12 +16,13 @@ function getTile(char: string) {
     case "D":
       return { x: 5, y: 1 };
     case "P":
-      return { x: 1, y: 7 };
+      return { x: 1, y: 1 };
     default:
       return { x: 1, y: 1 };
   }
 }
 export default class LevelMap extends TileMap {
+  playerPos: Vec2;
   constructor() {
     const tileW = 64;
     const tileH = 64;
@@ -44,13 +45,20 @@ BBBBBBBBBBBB`;
       .split("\n")
       .slice(1)
       .map((row) => row.split(""));
+    let playPos: Vec2 = new Vec2(0, 0);
 
     for (let i = 0; i < cells.length; i++) {
       for (let j = 0; j < cells[i].length; j++) {
-        level[i * cells[i].length + j] = getTile(cells[i][j]);
+        const tile = getTile(cells[i][j]);
+        level[i * cells[i].length + j] = tile;
+        if (cells[i][j] == "P") {
+          playPos = new Vec2(j * 64, i * 64);
+        }
       }
     }
 
     super(level, new Texture("dungeon.png"), mapW, mapH, tileW, tileH);
+
+    this.playerPos = playPos;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,23 @@ import renderUpdate from "./loop";
 
 const controls = new KeyControls();
 
-const tile = new TileSprite(new Texture("spider10.png"), 64, 64, 64, 64, 0, 0, {
-  x: 0,
-  y: 0,
-});
 const map = new LevelMap();
+const tile = new TileSprite(
+  new Texture("spider10.png"),
+  64,
+  64,
+  64,
+  64,
+  map.playerPos.x,
+  map.playerPos.y,
+  {
+    x: 0,
+    y: 0,
+  }
+);
 function render(ctx: CanvasRenderingContext2D) {
-  //   tile.render(ctx)
   map.render(ctx);
+  tile.render(ctx);
 }
 
 function update(dt: number, t: number) {}
