07d8ff34b6eec79cb8d7d380b80162c751a2cccf
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-11-28 15:47:01 +0630

    walker

--- src/Walker.ts
+++ src/Walker.ts
@@ -6,23 +6,19 @@ export default class Walker {
   render(ctx: CanvasRenderingContext2D) {
     ctx.save();
     ctx.beginPath();
-    ctx.fillStyle = 'black';
-    ctx.arc(this.x, this.y, 1, 0, Math.PI * 2, false);
+    ctx.fillStyle = 'red';
+    ctx.arc(this.x, this.y, 2, 0, Math.PI * 2, false);
     ctx.fill();
     ctx.closePath();
     ctx.restore();
   }
 
   update(dt: number) {
-    const choice = getRandomNumber(4);
-    if (choice == 0) {
-      this.x++;
-    } else if (choice == 1) {
-      this.x--;
-    } else if (choice == 2) {
-      this.y++;
-    } else {
-      this.y--;
-    }
+    const stepX = getRandomNumber(3) - 1;
+    const stepY = getRandomNumber(3) - 1;
+
+    this.x += stepX * 2;
+    this.y += stepY * 2;
+    
   }
 }
